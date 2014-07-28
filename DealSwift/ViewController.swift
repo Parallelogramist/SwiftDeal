a3b6704aa76bcb88b005ae69143bf70bab8ef641
Developer <Developer@MacBook-Air-Power.local>
2014-07-28 09:11:01 -0700

    Initial Commit

---
+++ DealSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DealSwift
+//
+//  Created by Developer on 7/28/14.
+//  Copyright (c) 2014 Osciciol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
